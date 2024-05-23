d761455c68bd38e780ea6dde97efe09b4bfeae29
小马同学 <89441679+m2075498242@users.noreply.github.com>
2024-05-23 15:51:28 +0800

    路径别名配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
+import { resolve } from "path"
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 配置路径别名
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
 })
