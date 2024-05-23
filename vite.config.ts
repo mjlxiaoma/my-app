cd84e0fe2799525a58223f0827e2d031766e4a63
小马同学 <89441679+m2075498242@users.noreply.github.com>
2024-05-23 15:54:48 +0800

    axios设置以及跨域配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,22 @@
-import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
-import { resolve } from "path"
+import { defineConfig, loadEnv } from "vite"
 
-export default defineConfig({
-  plugins: [react()],
-  // 配置路径别名
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "src"),
+export default defineConfig(({ mode }) => {
+  const config = loadEnv(mode, "./")
+
+  return {
+    plugins: [react()],
+    // 配置跨域
+    server: {
+      port: 8888,
+      host: "192.168.31.207",
+      proxy: {
+        "/api": {
+          target: config.VITE_TARGET,  // 会自动判断当前环境，对应上述三个文件中的target
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
     },
-  },
+  }
 })
